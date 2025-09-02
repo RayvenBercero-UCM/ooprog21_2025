1298e2c50e5caa08232ba80c897f9fda865b08e3
RayvenBercero-UCM <r4ayven@gmail.com>
2025-09-02 19:06:35 +0800

    Update TableAndChairs.java

--- Chapter_1/TableAndChairs.java
+++ Chapter_1/TableAndChairs.java
@@ -2,24 +2,24 @@ public class TableAndChairs {
    public static void main(String[] args) {
       int i;
       for (i = 1; i <= 5; i++) {
-         System.out.println("|                                |");
+         System.out.println("x                                   x");
          }
-         System.out.print("|");
-      for (i = 1; i <= 6; i++) 
+         System.out.print("x");
+      for (i = 1; i <= 7; i++) 
          {
-         System.out.print("_");
+         System.out.print("x");
          }
          System.out.print("      ");
       for (i = 1; i <= 8; i++) {
-         System.out.print("_");
+         System.out.print("x");
          }
-         System.out.print("     ");
-      for (i = 1; i <= 7; i++) {
-         System.out.print("_");
+         System.out.print("      ");
+      for (i = 1; i <= 8; i++) {
+         System.out.print("x");
          }
-         System.out.println("|");      
+         System.out.println("x");      
       for (i = 1; i <= 4; i++) {
-            System.out.println("|      |     |      |    |       |");
+            System.out.println("x      x      x      x      x       x");
         }
 
       
