233cd4994b1d931bcd411f5a8f1cd074037cf855
RayvenBercero-UCM <r4ayven@gmail.com>
2025-09-06 15:12:00 +0800

    Add files via upload

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,26 @@
+public class FixDebugOne
+{
+   /* This program displays a greeting */
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+   }
+}
+
+/*
+List down the spotted bugs below:
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+Bugs:
+1. class name has space which is invalid.. previous class name: FixDebug One
+
+2. letter M on the main method should be in lowercase
+
+3. There is no brackets on String[] args parameter
+
+4. Missing of letter "e" in system, should be: System.out.println
+
+5. lack of semi-colon at the end of the code, it shouldbe ("Hello World!");
+
+6. no close curly bracket on the last part
+*/
