8af573e9c5bf8057b62ab1c3af14979abb62c474
RayvenBercero-UCM <r4ayven@gmail.com>
2025-09-23 22:56:52 +0800

    Create DebugCircle.java

---
+++ Chapter_4/DebugCircle.java
@@ -0,0 +1,31 @@
+public class DebugCircle
+{
+   private static int radius;
+   private static int diameter;
+   private static final double PI = 3.14159;
+   private static double area;
+   private static int r;
+   
+   public static void main(String[] args){
+      DebugCircle(r);
+   }
+   public static int DebugCircle(int r)
+   {
+      radius = r;
+      diameter = 2 * r;
+      area = PI * r * r;
+      return r;
+   }
+   public static int getRadius(int radius)
+   {
+      return radius;
+   }
+   public static int getDiameter(int diameter)
+   {
+      return diameter;
+   }
+   public static double getArea(double area)
+   {
+      return area;
+   }
+}
