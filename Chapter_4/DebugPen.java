af2f3f1393a2426a97b447f1e94c809012bf8b29
RayvenBercero-UCM <r4ayven@gmail.com>
2025-09-29 08:32:41 +0800

    Update DebugPen.java

--- Chapter_4/DebugPen.java
+++ Chapter_4/DebugPen.java
@@ -2,18 +2,17 @@ public class DebugPen
 {
    private String color;
    private String point;
-
+   
    public DebugPen()
    {
       color = "black";
       point = "fine";
    }
-   public String DebugPen(String color, String point)
+   public DebugPen(String color, String point)
    {
-      color = color;
-      point = point;
-      return color;
-   }
+      this.color = color;
+      this.point = point;
+   }   
    public String getColor()
    {
       return color;
@@ -23,4 +22,3 @@ public class DebugPen
       return point;
    }
 }
-
