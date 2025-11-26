6df13c6343af8e71208a9894d12949740b2ce7c2
RayvenBercero-UCM <r4ayven@gmail.com>
2025-11-26 08:37:13 +0800

    Update StringBuilderDemo.java

--- Chapter_7/StringBuilderDemo.java
+++ Chapter_7/StringBuilderDemo.java
@@ -8,13 +8,13 @@ class StringBuilderDemo {
       
       StringBuilder addressString = new StringBuilder("6311 Hickory Nut Grove Road");
 
-      System.out.println("\n(String Builder 2) addressString: " + nameString);
+      System.out.println("\n(String Builder 2) addressString: " + addressString);
       System.out.println("The capacity of nameString is: " + addressString.capacity());
       
       nameString.setLength(20);
       addressString.setLength(20);
       
-      System.out.println("\n(Length set to 20 for String Builder 1) The name is  " + nameString + "end");
+      System.out.println("\n(Length set to 20 for String Builder 1) The name is " + nameString + "end");
       System.out.println("(Length set to 20 for String Builder 2) The address is " + addressString);
       
    }
